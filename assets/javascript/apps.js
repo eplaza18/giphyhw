e85879528f4bf3f6cfc6eac943e3b775a8013f7f
erikplaza@gmail.com <erikplaza@gmail.com>
2019-03-01 13:31:13 -0800

    Got JS to work mostly right, lots of little bugs to fix.

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -2,41 +2,85 @@ $(function() {
 console.log('Hello')
 
 //Array of items
-let topics = ['camp', 'hunt', 'backpacking']
-
+let topics = ['camp', 'hunting', 'backpacking']
+renderButtons();
 //Giphy API link key = dc6zaTOxFJmzC
 // Still gif = fixed_height_still
 
 // function displayGifs () {
 // var gifSearch = $(this).attr("data-name");
+function addGifs() {
+  
+  let gif = $(this).attr('data-name');
+  var queryURL = 'http:api.giphy.com/v1/gifs/search?q=' + gif + '&api_key=dc6zaTOxFJmzC&limit=10&fixed_height_still';
 
-
-var queryURL = 'http:api.giphy.com/v1/gifs/search?q=hunting&api_key=dc6zaTOxFJmzC&limit=10&fixed_height_still';
-
-$.ajax({
+  $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
+  })
 
+  .then(function(response) {
 
-    console.log(response);
+    var imageUrl = response.data;
+    console.log(imageUrl);
+
+    for (i = 0; i < imageUrl.length; i++) {
+      var gifContainer = $('<div class = "images">');
+      var rating = imageUrl[i].rating;
+      var ratingText = $('<h2>').text(`Rating: ${rating}`);
+      var gifImage = $(`<img stillSrc = "${imageUrl[i].images.fixed_height_still.url}" 
+        animatedSrc = "${imageUrl[i].images.fixed_height.url}"
+        src = "${imageUrl[i].images.fixed_height_still.url}">`);
+      gifContainer.append(ratingText);
+      gifContainer.append(gifImage);
+      $('#gif-view').prepend(gifContainer);
+
+    }
   });
-
-
+}
+    // gifImage.attr('src', imageUrl);
+    // gifImage.attr('alt', gif);
+    // $('#gif-view').append(gifImage);
 //Creates buttons for items in array
 function renderButtons() {
 
-    // Loops through arrays
+    $('#button-view').empty();
     for (i = 0; i < topics.length; i++){
-
       var a = $("<button>");
-      
       a.addClass("gifbut");
       a.attr("data-name",topics[i]); 
       a.text(topics[i]);
-      $("#buttons").append(a);
+      $("#button-view").append(a); 
     }
   }
-  renderButtons();
+
+  //Adds item from the search box into the array for a new button
+  $('#add-gif').on('click', function(event){
+    event.preventDefault();
+    let gif = $('#gif-input').val().trim();
+    topics.push(gif);
+    renderButtons(); 
+  });
+
+  //Resets buttons to work after new one is generated
+  $(document).on("click", ".gifbut", addGifs);
+    
+
+  //Changes status from animated to still and vice versa  
+  $(document.body).on("click", "img", function() { 
+      var isAnimated = $(this).attr("isAnimated");
+      if(isAnimated==null || isAnimated=='' ){
+        isAnimated =  "false";
+      }
+  
+      if(isAnimated=="true"){
+        $(this).attr("src",  $(this).attr("stillSrc") );
+        $(this).attr("isAnimated", "false");
+  
+      } else {
+        $(this).attr("src",  $(this).attr("animatedSrc") );
+        $(this).attr("isAnimated", "true");
+      }
+  });
   
 });
